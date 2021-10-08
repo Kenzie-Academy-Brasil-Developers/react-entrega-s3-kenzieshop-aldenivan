6b29406aa3abb01da3c249dfbd9c544041834a3b
Aldenivan Jr <aldenivan1207@gmail.com>
2021-10-08 11:47:08 -0300

    Mounting poduct list

---
+++ src/components/defaultState.js
@@ -0,0 +1,59 @@
+export const defaultState = [
+  {
+    id: 1,
+    product: "Sony PlayStation 5 825GB Standard cor branco e preto",
+    price: 5280,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_669939-MLA44492818154_012021-O.webp",
+  },
+  {
+    id: 2,
+    product:
+      "Sony PlayStation 4 Slim 1TB Mega Pack: Ghost of Tsushima/God of War/Ratchet & Clank cor preto onyx",
+    price: 2999,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_789653-MLA46869170326_072021-O.webp",
+  },
+  {
+    id: 3,
+    product: "Sony PlayStation 3 Slim 250GB Standard cor charcoal black",
+    price: 1399,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_790578-MLA40694915968_022020-O.webp",
+  },
+  {
+    id: 4,
+    product: "Microsoft Xbox 360 + Kinect Slim 4GB Standard cor matte black",
+    price: 5.28,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_749276-MLA32731207914_112019-O.webp",
+  },
+  {
+    id: 5,
+    product: "Console Microsoft Xbox Series X 1tb Ssd 4k 120fps 8k Hdr",
+    price: 5999.99,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_794435-MLA45046407331_032021-O.webp",
+  },
+  {
+    id: 6,
+    product: "Sony PlayStation Classic 16GB cor cinza",
+    price: 618,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_869142-MLA32731749132_112019-O.webp",
+  },
+  {
+    id: 7,
+    product: "Sony PlayStation 2 + 1 Controle",
+    price: 599,
+    image:
+      "https://cdn.awsli.com.br/600x450/138/138431/produto/11377588/7eb4596ac0.jpg",
+  },
+  {
+    id: 8,
+    product: "Nintendo Super NES Classic Edition 512MB cor cinza e violeta",
+    price: 5.28,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_922126-MLA32731490477_112019-O.webp",
+  },
+];
