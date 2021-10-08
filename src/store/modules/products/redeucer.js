68df3db9f21479bf2f6e017a1591ab1b876f0bf7
Aldenivan Jr <aldenivan1207@gmail.com>
2021-10-08 11:48:05 -0300

    Mounting products module

---
+++ src/store/modules/products/redeucer.js
@@ -0,0 +1,7 @@
+import { defaultState } from "../../../components/defaultState";
+
+const productsReducer = (state = defaultState, action) => {
+  return state;
+};
+
+export default productsReducer;
