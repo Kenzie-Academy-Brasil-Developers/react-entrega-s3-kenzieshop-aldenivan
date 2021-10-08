114f8c020372f521506761bfa1d5253ec3698195
Aldenivan Jr <aldenivan1207@gmail.com>
2021-10-08 11:55:12 -0300

    Mounting product list component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <ProductList />
+      </header>
     </div>
   );
 }
--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,3 +1,20 @@
-const ProductList = () => {};
+import { useSelector } from "react-redux";
+import Product from "../Product";
+
+const ProductList = () => {
+  const { products } = useSelector((store) => store);
+
+  return (
+    <>
+      <div>
+        {products.map((product) => (
+          <li>
+            <Product product={product} />
+          </li>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default ProductList;
